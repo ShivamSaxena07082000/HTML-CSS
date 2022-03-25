005fa889ca5c7945644e8e5e6aed4949f38264f3
Shivam Saxena <69047409+ShivamSaxena07082000@users.noreply.github.com>
2022-03-25 17:20:23 +0530

    Javascript

---
+++ marksheet.js
@@ -0,0 +1,15 @@
+function ss()
+{
+var a = document.getElementById('rol');
+if(a.value == '')
+    {
+    alert('Please Enter a Roll Number');
+    }
+else
+{
+  if(a.value != '')
+  {
+        
+}    
+}    
+}
